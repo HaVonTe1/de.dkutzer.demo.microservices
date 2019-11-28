3b534cf048074d3eee1c3843d64430d1059d2aed
Dirk Kutzer <dirk.kutzer@dbfuhrpark.com>
2019-11-28 09:31:37 +0100

    project setup for developer service with quarkus.io

---
+++ developer-quarkus/src/test/java/de/dkutzer/buggy/NativedeveloperIT.java
@@ -0,0 +1,9 @@
+package de.dkutzer.buggy;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativedeveloperIT extends developerTest {
+
+    // Execute the same tests but in native mode.
+}
