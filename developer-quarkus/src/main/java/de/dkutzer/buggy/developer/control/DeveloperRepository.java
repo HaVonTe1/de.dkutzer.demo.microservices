9d6f4e22e04c826a69396f38cd1986c328a2ffc3
Dirk Kutzer <kutzer@365farmnet.com>
2019-12-17 12:17:08 +0100

    #11 first success with native images ... oh boy!

--- developer-quarkus/src/main/java/de/dkutzer/buggy/developer/control/DeveloperRepository.java
+++ developer-quarkus/src/main/java/de/dkutzer/buggy/developer/control/DeveloperRepository.java
@@ -4,9 +4,17 @@ import com.google.common.collect.Lists;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.UpdateOptions;
 import de.dkutzer.buggy.developer.entity.Developer;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -14,22 +22,64 @@ import javax.inject.Inject;
 @ApplicationScoped
 public class DeveloperRepository
 {
+    public static final String DEVELOPERS = "developers";
+    public static final String BUGGY_DB = "buggy";
+
     @Inject
     MongoClient mongoClient;
+
+    private UpdateOptions upsertOptions = new UpdateOptions().upsert(true);
 
 
     public Iterable<Developer> findAll() {
 
-        final MongoCollection<Document> developersCol = mongoClient.getDatabase("buggy").getCollection("developers");
+        final MongoCollection<Document> developersCol = getCollection();
         final FindIterable<Document> documents = developersCol.find();
         List<Developer> result = Lists.newArrayList();
         for(Document doc : documents){
-            final Developer e = new Developer();
-            e.setId(doc.getString("id"));
-            e.setFirstName(doc.getString("firstName"));
-            e.setLastName(doc.getString("lastName"));
+            final Developer e = getDeveloperByDocument(doc);
             result.add(e);
         }
         return result;
+    }
+
+    private Developer getDeveloperByDocument(Document doc) {
+        final Developer e = new Developer();
+        e.setId(doc.getString("id"));
+        e.setFirstName(doc.getString("firstName"));
+        e.setLastName(doc.getString("lastName"));
+        return e;
+    }
+
+    public Optional<Developer> findById(final String id){
+        MongoCollection<Document> collection = getCollection();
+        FindIterable<Document> iterable = collection.find(new Document("id", id));
+        if(iterable.iterator().hasNext()){
+            Document document = iterable.first();
+            return Optional.of(getDeveloperByDocument(document));
+        }
+        return Optional.empty();
+    }
+
+    private MongoCollection<Document> getCollection() {
+        return mongoClient.getDatabase(BUGGY_DB).getCollection(DEVELOPERS);
+    }
+
+    public void upsert(final Developer developer){
+
+        MongoCollection<Document> collection = getCollection();
+        collection.updateOne(getFilterByDeveloper(developer), getDocumentByDeveloper(developer), upsertOptions);
+    }
+
+    private Document getDocumentByDeveloper(Developer developer) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", developer.getId());
+        map.put("firstName", developer.getFirstName());
+        map.put("lastName",developer.getLastName());
+        return new Document(map);
+    }
+
+    private Bson getFilterByDeveloper(Developer developer) {
+        return new Document("id", developer.getId());
     }
 }
