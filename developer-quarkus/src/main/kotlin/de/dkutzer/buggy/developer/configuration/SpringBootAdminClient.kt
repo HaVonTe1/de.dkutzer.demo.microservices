49dfe65debe2c60f80f25adc54ec942b302f8708
Dirk Kutzer <kutzer@365farmnet.com>
2020-01-10 16:10:55 +0100

    #11 adding health endpoint and preparing sba client

---
+++ developer-quarkus/src/main/kotlin/de/dkutzer/buggy/developer/configuration/SpringBootAdminClient.kt
@@ -0,0 +1,40 @@
+package de.dkutzer.buggy.developer.configuration
+
+import com.google.common.collect.Maps
+import io.quarkus.scheduler.Scheduled
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Default
+import javax.inject.Inject
+import javax.ws.rs.core.MediaType
+
+
+@ApplicationScoped
+class HeartbeatScheduledController {
+
+    data class StatusDTO(val name: String,val  managementUrl: String,val healthUrl: String,val serviceUrl:String,val  metaData: Map<String, String>)
+
+    @Inject
+    @ConfigProperty(name = "quarkus.application.name", defaultValue = "buggy-developer-service")
+    @field: Default
+    lateinit var appName: String
+
+    @Inject
+    @ConfigProperty(name = "quarkus.application.uri", defaultValue = "http://buggy-developer-quarkus-service:8080/")
+    @field: Default
+    lateinit var  appuri: String
+
+    @Inject
+    @field: Default
+    lateinit var restClient: SpringBootAdminRestClient
+
+    @Scheduled(every="10s")
+    fun postStatusToSBA(){
+        val statusDTO = StatusDTO(appName, appuri, appuri+"health", appuri, Maps.newHashMap())
+
+        restClient.postStatus(MediaType.APPLICATION_JSON,MediaType.APPLICATION_JSON, statusDTO);
+
+
+
+    }
+}
