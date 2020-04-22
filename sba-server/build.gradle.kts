8add537884729caffec49be589d3c460dada82f0
Dirk Kutzer <kutzer@365farmnet.com>
2020-04-22 15:21:28 +0200

    more kubernetes stuff

--- sba-server/build.gradle.kts
+++ sba-server/build.gradle.kts
@@ -1,15 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.0.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.3.50"
-    kotlin("plugin.spring") version "1.3.50"
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.spring") version "1.3.71"
 }
 
 group = "de.dkutzer.buggy"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+extra["springBootAdminVersion"] = "2.2.1"
 
 repositories {
     mavenCentral()
@@ -23,7 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("de.codecentric:spring-boot-admin-starter-server:2.2.0-SNAPSHOT")
+    implementation("de.codecentric:spring-boot-admin-starter-server:${property("springBootAdminVersion")}")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
